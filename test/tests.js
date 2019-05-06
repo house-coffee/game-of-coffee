a3db7cfc8a7f3b9b85baea798829105eb7adfe52
Lili Boxer <ninaliboxer@gmail.com>
2019-05-06 13:23:33 -0700

    tdd make user function

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,42 @@
 const test = QUnit.test;
 
+function makeUser(formData) {
+    const user = {
+        name: formData.get('name'),
+        house: formData.get('house'),
+        army: 100,
+        morale: 50,
+        caffeinated: formData.get('caffeinated')
+    };
+
+    return user;
+}
+
+
+
 test('time to test a function', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const formData = new FormData();
+    formData.set('name', 'LA');
+    formData.set('house', 'Stark');
+    formData.set('army', 100);
+    formData.set('morale', 50);
+    formData.set('caffeinated', 'yes');
+
+    const expected = {
+        name: 'LA',
+        house: 'Stark',
+        army: 100,
+        morale: 50,
+        caffeinated: 'yes',
+    };
 
     //Act 
     // Call the function you're testing and set the result to a const
 
+    const user = makeUser(formData);
+
     //Assert
-    assert.equal(true, false);
+    assert.deepEqual(user, expected);
 });
