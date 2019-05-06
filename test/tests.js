763ec06a2cf1bfc1a6681646e189608422721a33
Lili Boxer <ninaliboxer@gmail.com>
2019-05-06 14:12:09 -0700

    TDD API Utility

--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,5 @@
+import makeUser from '../src/make-user.js';
 const test = QUnit.test;
-
-function makeUser(formData) {
-    const user = {
-        name: formData.get('name'),
-        house: formData.get('house'),
-        army: 100,
-        morale: 50,
-        caffeinated: formData.get('caffeinated')
-    };
-
-    return user;
-}
-
-
 
 test('time to test a function', function(assert) {
     //Arrange
