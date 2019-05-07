4aa1212402fe24a0deb6a7ba08a0b3b150b00592
Lili Boxer <ninaliboxer@gmail.com>
2019-05-07 14:53:30 -0700

    added links on map.html, started creat quest link and creat quest link test

---
+++ test/create-quest-link.test.js
@@ -0,0 +1,19 @@
+
+const test = QUnit.test;
+
+QUnit.module('create quest link');
+
+
+test('creates correct link html', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+
+ 
+
+    //Assert
+    assert.deepEqual(user, expected);
+});
