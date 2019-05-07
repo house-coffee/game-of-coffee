72f44b4d63f2ea530a8b79494919e6de3ee9fd59
Lili Boxer <ninaliboxer@gmail.com>
2019-05-07 15:23:40 -0700

    create quest link TDD complete

--- test/create-quest-link.test.js
+++ test/create-quest-link.test.js
@@ -3,17 +3,34 @@ const test = QUnit.test;
 
 QUnit.module('create quest link');
 
+function createQuestLink(quest) {
+    const link = document.createElement('a');
+    link.classList.add('quest');
 
-test('creates correct link html', function(assert) {
+    const searchParams = new URLSearchParams();
+    searchParams.set('id', quest.id);
+    link.href = 'quest.html' + searchParams.toString();
+
+    link.textContent = quest.title;
+
+    return link;
+}
+
+test('creates correct link html', (assert) => {
     //Arrange
     // Set up your parameters and expectations
-    
+    const quest = {
+        id: 'blackwater',
+        title: 'Battle of Blackwater',
+    };
+    const expected = '<a class="quest" href="quest.htmlid=blackwater">Battle of Blackwater</a>';
 
-    //Act 
+    //Act
+
     // Call the function you're testing and set the result to a const
-
+    const dom = createQuestLink(quest);
  
-
     //Assert
-    assert.deepEqual(user, expected);
+    assert.equal(dom.outerHTML, expected);
+
 });
