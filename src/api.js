227f3a9116064859978e2292d7b88870d0340e3b
Lili Boxer <ninaliboxer@gmail.com>
2019-05-07 17:51:35 -0700

    maybe we are halfway? we need to fix out getQuest function

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import questData from './quests/quest-data.js';
+import findById from './find-by-id.js';
 
 const api = {
     storage: localStorage,
@@ -14,6 +15,9 @@ const api = {
     },
     getQuests() {
         return questData;
+    },
+    getQuest(id) {
+        return findById(questData, id);
     }
 };
 export default api;
--- src/quests/quest.js
+++ src/quests/quest.js
@@ -1,11 +1,13 @@
 import loadProfile from '../../map/load-profile.js';
 import api from '../api.js';
+import findById from '../find-by-id.js';
 
 loadProfile();
 
 const searchParams = new URLSearchParams(window.location.search);
 const questId = searchParams.get('id');
-const quest = api.getQuests(questId);
+const quest = api.getQuest(questId);
+console.log(quest);
 
 if(!quest) {
     window.location = 'map.html';
@@ -13,10 +15,11 @@ if(!quest) {
 
 const title = document.getElementById('title');
 const image = document.getElementById('image');
-const choices = document.getElementById('choices');
 const description = document.getElementById('description');
-const result =  document.getElementById('results');
+const result = document.getElementById('result');
+const choiceForm = document.getElementById('choice-form');
+const choices = document.getElementById('choices');
 
 title.textContent = quest.title;
-// image.src = 'assets/' +
+image.src = 'assets/' + quest.image;
 description.textContent = quest.description;
